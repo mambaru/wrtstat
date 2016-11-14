37aea1ab6ea86bc8e91fd37d8ed7a78f44624445
Vladimir Migashko <migashko@gmail.com>
2016-11-14 19:59:03 +0300

    ups

--- wrtstat/wrtstat.hpp
+++ wrtstat/wrtstat.hpp
@@ -58,45 +58,6 @@ public:
     return _ag->reg_name(name, now);
   }
 
-  
-  //typedef std::chrono::high_resolution_clock clock_type;
-  /*
-  typedef std::function< void(time_type now, time_type v) > timer_fun_t;
-
-  template<typename D>
-  struct time_meter
-  {
-    typedef D duration_type;
-
-    time_meter(time_type now, timer_fun_t fun, mutex_ptr pmutex)
-      : now(now)
-      , timer_fun(fun)
-      , wmutex(pmutex)
-    {
-      start = clock_type::now();
-    }
-
-    ~time_meter()
-    {
-      if (timer_fun==nullptr)
-        return;
-      if ( auto pmutex = wmutex.lock() ) 
-      {
-        clock_type::time_point finish = clock_type::now();
-        time_type span = std::chrono::template duration_cast<D>( finish - start ).count();
-        std::lock_guard<mutex_type> lk(*pmutex);
-        timer_fun( now, span );
-      }
-    };
-
-    time_type now;
-    timer_fun_t timer_fun;
-    std::weak_ptr<mutex_type> wmutex;
-    clock_type::time_point start;
-  };
-  */
-  //typedef std::shared_ptr<time_meter> time_meter_ptr;
-
   template<typename D>
   std::shared_ptr< time_meter<D> > create_handler(int id, time_type now)
   {
