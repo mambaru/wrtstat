900294f1b28601fe1c6932988bc35cc43af7ee06
Vladimir Migashko <migashko@gmail.com>
2016-12-14 17:32:30 +0300

    +test

---
+++ tests/wrtstat_suite.cpp
@@ -0,0 +1,31 @@
+#include <fas/testing.hpp>
+#include <wrtstat/wrtstat.hpp>
+#include <chrono>
+
+namespace {
+
+UNIT(wrtstat1, "")
+{
+  using namespace fas::testing;
+  using namespace wrtstat;
+  wrtstat_mt stat;
+  stat.create_multi_meter<std::chrono::microseconds>(
+    "a1", "a2", "a3", 
+    wrtstat_mt::now<std::chrono::microseconds>(), 
+    1, 255);
+  
+  stat.create_multi_meter< size_meter >( "s1", wrtstat_mt::now<std::chrono::microseconds>(), 12);
+  stat.create_multi_meter< value_meter >( "v1", wrtstat_mt::now<std::chrono::microseconds>(), 12);
+  stat.create_multi_meter< value_meter >( "v2", wrtstat_mt::now<std::chrono::microseconds>(), 12, 1);
+  
+  stat.create_multi_meter< time_meter<std::chrono::nanoseconds> >( "t1", wrtstat_mt::now<std::chrono::microseconds>());
+  stat.create_multi_meter< time_meter<std::chrono::nanoseconds> >( "t2", wrtstat_mt::now<std::chrono::microseconds>(), 10 );
+  t << nothing;
+}
+
+}
+
+BEGIN_SUITE(wrtstat, "")
+  ADD_UNIT(wrtstat1)
+END_SUITE(wrtstat)
+
