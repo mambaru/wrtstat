c0d259c4203fc5b056425c9d3f3b3eb3586ef822
Vladimir Migashko <migashko@gmail.com>
2018-01-26 21:20:13 +0300

    ci fix

--- wrtstat/aggregated_data.hpp
+++ wrtstat/aggregated_data.hpp
@@ -29,16 +29,16 @@ inline aggregated_info& aggregated_info::operator+=(const aggregated_info& v)
   typedef std::pair<value_type, value_type> vp_t;
   std::array< vp_t, 10 > arr1 = 
   {
-    vp_t{ perc50*(count/2+1),    value_type(perc50)},
-    vp_t{ perc80*(count/3+1),    perc80},
-    vp_t{ perc95*(count/6+1),    perc95},
-    vp_t{ perc99*(count/10+1),   perc99},
-    vp_t{ perc100*(count/100+1), perc100},
-    vp_t{ v.perc50*(v.count/2+1),    v.perc50},
-    vp_t{ v.perc80*(v.count/3+1),    v.perc80},
-    vp_t{ v.perc95*(v.count/6+1),    v.perc95},
-    vp_t{ v.perc99*(v.count/10+1),   v.perc99},
-    vp_t{ v.perc100*(v.count/100+1), v.perc100}
+    vp_t{ value_type(perc50*(count/2+1) ),    perc50},
+    vp_t{ value_type(perc80*(count/3+1) ),    perc80},
+    vp_t{ value_type(perc95*(count/6+1) ),    perc95},
+    vp_t{ value_type(perc99*(count/10+1) ),   perc99},
+    vp_t{ value_type(perc100*(count/100+1) ), perc100},
+    vp_t{ value_type(v.perc50*(v.count/2+1) ),    v.perc50},
+    vp_t{ value_type(v.perc80*(v.count/3+1) ),    v.perc80},
+    vp_t{ value_type(v.perc95*(v.count/6+1) ),    v.perc95},
+    vp_t{ value_type(v.perc99*(v.count/10+1) ),   v.perc99},
+    vp_t{ value_type(v.perc100*(v.count/100+1) ), v.perc100}
   };
   
   std::sort(arr1.begin(), arr1.end());
