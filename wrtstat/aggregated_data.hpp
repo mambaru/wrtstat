61350a0577080b01421bd0fd242bbea13c5c931e
Vladimir Migashko <migashko@gmail.com>
2018-01-26 22:16:06 +0300

    clang++ fix

--- wrtstat/aggregated_data.hpp
+++ wrtstat/aggregated_data.hpp
@@ -29,22 +29,28 @@ inline aggregated_info& aggregated_info::operator+=(const aggregated_info& v)
   typedef std::pair<value_type, value_type> vp_t;
   value_type count1 = static_cast<value_type>(this->count);
   value_type count2 = static_cast<value_type>(v.count);
-  std::array< vp_t, 10 > arr1 = 
-  {
-    vp_t(perc50*(count1/2+1),    perc50),
-    vp_t(perc80*(count1/3+1),    perc80),
-    vp_t(perc95*(count1/6+1),    perc95),
-    vp_t(perc99*(count1/10+1),   perc99),
-    vp_t(perc100*(count1/100+1), perc100),
-    vp_t(v.perc50*(count2/2+1),    v.perc50),
-    vp_t(v.perc80*(count2/3+1),    v.perc80),
-    vp_t(v.perc95*(count2/6+1),    v.perc95),
-    vp_t(v.perc99*(count2/10+1),   v.perc99),
-    vp_t(v.perc100*(count2/100+1), v.perc100)
-  };
+  std::array< vp_t, 10 > arr1;
+  
+  arr1[0].first = perc50*(count1/2+1);     arr1[0].second = perc50;
+  arr1[1].first = perc80*(count1/3+1);     arr1[1].second = perc80;
+  arr1[2].first = perc95*(count1/6+1);     arr1[2].second = perc95;
+  arr1[3].first = perc99*(count1/10+1);     arr1[3].second = perc99;
+  arr1[4].first = perc100*(count1/100+1);     arr1[4].second = perc100;
+  arr1[5].first = v.perc50*(count2/2+1);     arr1[5].second = v.perc50;
+  arr1[6].first = v.perc80*(count2/3+1);     arr1[6].second = v.perc80;
+  arr1[7].first = v.perc95*(count2/6+1);     arr1[7].second = v.perc95;
+  arr1[8].first = v.perc99*(count2/10+1);     arr1[8].second = v.perc99;
+  arr1[9].first = v.perc100*(count2/100+1);     arr1[9].second = v.perc100;
+  
   
   std::sort(arr1.begin(), arr1.end());
-  std::array< value_type, 5 > arr2 = { arr1[3].second, arr1[6].second, arr1[7].second, arr1[8].second, arr1[9].second};
+  std::array< value_type, 5 > arr2;
+  arr2[0] = arr1[3].second;
+  arr2[1] = arr1[6].second; 
+  arr2[2] = arr1[7].second; 
+  arr2[3] = arr1[8].second;
+  arr2[4] = arr1[9].second;
+  
   std::sort(arr2.begin(), arr2.end());
   perc50 = arr2[0];
   perc80 = arr2[1];
