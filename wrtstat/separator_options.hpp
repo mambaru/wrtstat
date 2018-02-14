de918de9cb32ad3af0658d97ab17eb7a32f7ef1c
Vladimir Migashko <migashko@gmail.com>
2018-02-14 18:32:58 +0300

    *resolutions fix

--- wrtstat/separator_options.hpp
+++ wrtstat/separator_options.hpp
@@ -10,7 +10,7 @@ struct separator_options
   // шаг обрезания. Если нужен шаг обрезания 5 сек для микросекунд (resolution=1000000), то нужно указать 5000000
   time_type aggregation_step_ts = 1;
   // resolution 1, 1000, 1000000, 1000000000
-  time_type resolution = 0;
+  time_type resolution = 1;
   // смещает начальное значение для того чтобы аггрегация не шла лавиной
   // значение аналогично step_ts, обычно достаточно чтобы было равным step_ts
   time_type soiled_start_ts = 0;
