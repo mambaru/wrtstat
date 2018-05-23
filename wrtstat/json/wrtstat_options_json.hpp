b5b0ad063965768dcb35d6b5d8489786a11cf0f2
Vladimir Migashko <migashko@gmail.com>
2018-05-23 11:38:14 +0300

    *json

--- wrtstat/json/wrtstat_options_json.hpp
+++ wrtstat/json/wrtstat_options_json.hpp
@@ -18,7 +18,7 @@ enum class resolutions
 
 */
 
-struct resolutions_json
+struct resolution_json
 {
   JSON_NAME(none)
   JSON_NAME(seconds)
@@ -64,14 +64,16 @@ struct reducer_options_json
 struct separator_options_json
 {
   JSON_NAME(aggregation_step_ts)
-  JSON_NAME(resolutions)
+  JSON_NAME(soiled_start_ts)
+  JSON_NAME(resolution)
 
   typedef wjson::object<
     separator_options,
     wjson::member_list<
       wjson::base<reducer_options_json>,
       wjson::member< n_aggregation_step_ts, separator_options, time_type, &separator_options::aggregation_step_ts>,
-      wjson::member< n_resolutions, separator_options, resolutions, &separator_options::resolution, resolutions_json>
+      wjson::member< n_soiled_start_ts, separator_options, time_type, &separator_options::soiled_start_ts>,
+      wjson::member< n_resolution, separator_options, resolutions, &separator_options::resolution, resolution_json>
     >,
     wjson::strict_mode
   > type;
