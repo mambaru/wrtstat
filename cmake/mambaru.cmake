ea346870303c5583d459fbfba609f77dfb78ff2a
Vladimir Migashko <migashko@gmail.com>
2018-06-29 18:56:21 +0300

    *cmake

--- cmake/mambaru.cmake
+++ cmake/mambaru.cmake
@@ -6,6 +6,17 @@ endif()
 include(mambaopt)
 include(mambalibs)
 
+
+if (NOT WRTSTAT_DISABLE_JSON OR BUID_TESTING)
+  get_faslib()
+endif()
+
+if ( WRTSTAT_DISABLE_JSON )
+  add_definitions(-DWRTSTAT_DISABLE_JSON)
+else()
+  get_mambaru(wjson WJSON_DIR "")
+endif()
+
 include_directories(${CMAKE_CURRENT_SOURCE_DIR})
 
 
