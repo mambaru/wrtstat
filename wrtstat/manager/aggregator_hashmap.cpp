adcbe0f514f2d5131edc6d6c1a0380b4cecc5e1a
Vladimir Migashko <migashko@gmail.com>
2019-08-26 18:17:14 +0300

    code clean. faslib up 0.8.2

--- wrtstat/manager/aggregator_hashmap.cpp
+++ wrtstat/manager/aggregator_hashmap.cpp
@@ -1,4 +1,5 @@
 #include <wrtstat/manager/aggregator_hashmap.hpp>
+#include <fas/system/memory.hpp>
 
 namespace wrtstat {
 
@@ -7,23 +8,14 @@ aggregator_hashmap::aggregator_hashmap(const options_type& opt )
 {
   this->initialize_();
 }
-  
-aggregator_hashmap::aggregator_hashmap(aggregator_hashmap&& agh) 
+
+aggregator_hashmap::aggregator_hashmap(aggregator_hashmap&& agh)
   : _hash( std::move(agh._hash) )
   , _opt( std::move(agh._opt) )
   ,_mutex_list( std::move(agh._mutex_list) )
   ,_aggregator_list( std::move(agh._aggregator_list) )
 {
 }
-  
-  /*
-aggregator_hashmap_mt::aggregator_hashmap_mt(const aggregator_hashmap_mt& agh)
-  : _hash(agh._hash)
-  , _opt(agh._opt)
-  , _hash_size(agh._hash_size)
-{
-  this->initialize_();
-}*/
 
 aggregator_hashmap& aggregator_hashmap::operator=(aggregator_hashmap&& agh)
 {
@@ -34,7 +26,7 @@ aggregator_hashmap& aggregator_hashmap::operator=(aggregator_hashmap&& agh)
   _aggregator_list = std::move(agh._aggregator_list);
   return *this;
 }
-  
+
 bool aggregator_hashmap::push( const std::string& name, const reduced_data& v, aggregated_handler handler)
 {
   size_t pos = this->get_pos_(name);
@@ -43,18 +35,18 @@ bool aggregator_hashmap::push( const std::string& name, const reduced_data& v, a
   std::lock_guard<mutex_type> lk( *mtx );
   return ag->push(name, v, std::move(handler) );
 }
-  
+
 size_t aggregator_hashmap::get_pos_( const std::string& name) const
 {
   return _hash(name) % _opt.hash_size;
 }
-  
+
 void aggregator_hashmap::initialize_()
 {
   _mutex_list.resize( _opt.hash_size);
   _aggregator_list.resize( _opt.hash_size);
-  for (auto& m : _mutex_list) m = mutex_ptr( new mutex_type() );
-  for (auto& m : _aggregator_list) m = aggregator_ptr(new aggregator_map(_opt));
+  std::generate(std::begin(_mutex_list), std::end(_mutex_list), std::make_unique<mutex_type>);
+  std::generate(std::begin(_aggregator_list), std::end(_aggregator_list), [this](){ return std::make_unique<aggregator_map>(_opt);});
 }
 
 }
--- wrtstat/manager/aggregator_registry.cpp
+++ wrtstat/manager/aggregator_registry.cpp
@@ -11,7 +11,7 @@ namespace wrtstat {
     , _pool(opt.reducer_limit, pool_size)
   { }
 
-  size_t aggregator_registry::aggregators_count() const 
+  size_t aggregator_registry::aggregators_count() const
   {
     read_lock<mutex_type> lk(_mutex);
     return _agarr.size();
@@ -23,12 +23,12 @@ namespace wrtstat {
       return p->add(now, v, count);
     return false;
   }
-  
+
   bool aggregator_registry::add( const std::string& name, const reduced_data& v)
   {
     if ( auto p = this->get_aggregator( name, v.ts ) )
       return p->add(v);
-    return false;  
+    return false;
   }
 
   aggregator_registry::aggregated_ptr aggregator_registry::force_pop(id_t id)
@@ -45,7 +45,7 @@ namespace wrtstat {
     return nullptr;
   }
 
-  std::string aggregator_registry::get_name(id_t id) const 
+  std::string aggregator_registry::get_name(id_t id) const
   {
     read_lock<mutex_type> lk(_mutex);
     return _dict.get_name(id);
@@ -55,13 +55,13 @@ namespace wrtstat {
   {
     if ( name.empty() )
       return bad_id;
-    
+
     std::lock_guard<mutex_type> lk(_mutex);
     id_t id = _dict.create_id( name );
     size_t pos = _dict.id2pos(id);
     if ( pos < _agarr.size() && _agarr[pos]!=nullptr )
       return id;
-    
+
     if ( _agarr.size() <= pos )
       _agarr.resize( pos + 1 );
 
@@ -73,17 +73,17 @@ namespace wrtstat {
     }
     return id;
   }
-  
+
   aggregator_registry::aggregator_ptr aggregator_registry::get_aggregator(const std::string& name, time_type now)
   {
     return this->get_aggregator( this->create_aggregator(name, now) );
   }
-  
+
   aggregator_registry::aggregator_ptr aggregator_registry::get_aggregator(id_t id) const
   {
     if ( id == bad_id )
       return nullptr;
-    
+
     read_lock<mutex_type> lk(_mutex);
     size_t pos = _dict.id2pos(id);
     if ( pos >= _agarr.size() )
@@ -93,90 +93,16 @@ namespace wrtstat {
     return _agarr[ pos ];
   }
 
-  /*
-  aggregator_registry::simple_adder_t aggregator_registry::create_simple_adder( id_t id )
-  {
-    if ( auto ag = this->get_aggregator(id) )
-      return ag->create_simple_adder();
-    return nullptr;
-  }
-
-  aggregator_registry::data_adder_t aggregator_registry::create_data_adder( id_t id )
-  {
-    if ( auto ag = this->get_aggregator(id) )
-      return ag->create_data_adder();
-    return nullptr;
-  }
-  
-  aggregator_registry::reduced_adder_t aggregator_registry::create_reduced_adder( id_t id )
-  {
-    if ( auto ag = this->get_aggregator(id) )
-      return ag->create_reduced_adder();
-    return nullptr;
-  }
-  
-  aggregator_registry::composite_adder_t aggregator_registry::create_composite_adder( id_t time_id, id_t read_id, id_t write_id, bool summary_size )
-  {
-    simple_adder_t time_adder;
-    simple_adder_t read_adder;
-    simple_adder_t write_adder;
-    
-    if ( auto ag = this->get_aggregator(time_id) )
-      time_adder = ag->create_simple_adder();
-
-    if ( auto ag = this->get_aggregator(read_id) )
-      read_adder = ag->create_simple_adder();
-
-    if ( auto ag = this->get_aggregator(write_id) )
-      write_adder = ag->create_simple_adder();
-
-    if (time_adder==nullptr && read_adder==nullptr && write_adder==nullptr)
-      return nullptr;
-    
-    return this->make_composite_handler_(time_adder, read_adder, write_adder, summary_size);
-  }
-
-  aggregator_registry::simple_adder_t aggregator_registry::create_value_adder(const std::string& name, time_type ts_now)
-  {
-    return this->create_simple_adder( this->create_aggregator(name, ts_now) );
-  }
-
-  aggregator_registry::data_adder_t aggregator_registry::create_data_adder(const std::string& name, time_type ts_now)
-  {
-    return this->create_data_adder( this->create_aggregator(name, ts_now));
-  }
-  
-  aggregator_registry::reduced_adder_t aggregator_registry::create_reduced_adder( const std::string& name, time_type ts_now )
-  {
-    return this->create_reduced_adder(this->create_aggregator(name, ts_now));
-  }
-  
-  aggregator_registry::composite_adder_t aggregator_registry::create_composite_adder( 
-    const std::string& time_name, 
-    const std::string& read_name, 
-    const std::string& write_name, 
-    bool summary_size,
-    time_type ts_now )
-  {
-    return this->create_composite_adder(
-      this->create_aggregator(time_name, ts_now),
-      this->create_aggregator(read_name, ts_now),
-      this->create_aggregator(write_name, ts_now),
-      summary_size
-    );
-  }
-  */
-  
   aggregator_registry::composite_adder_t
     aggregator_registry::make_composite_handler_(
-      simple_adder_t time_adder, 
-      simple_adder_t read_adder, 
-      simple_adder_t write_adder, 
-      bool summary_size  
+      simple_adder_t time_adder,
+      simple_adder_t read_adder,
+      simple_adder_t write_adder,
+      bool summary_size
     )
   {
     return [time_adder, read_adder, write_adder, summary_size]
-      (time_type now, time_type span, size_type count, 
+      (time_type now, time_type span, size_type count,
        size_type readed, size_type writed)
       {
         if ( time_adder!=nullptr )
@@ -188,7 +114,7 @@ namespace wrtstat {
       };
   }
 
-  
+
   aggregator_registry::simple_pusher_t aggregator_registry::create_simple_pusher( id_t id, aggregated_handler handler )
   {
     if ( auto ag = this->get_aggregator(id) )
@@ -202,7 +128,7 @@ namespace wrtstat {
       return ag->create_data_pusher(handler);
     return nullptr;
   }
-  
+
   aggregator_registry::reduced_pusher_t aggregator_registry::create_reduced_pusher( id_t id, aggregated_handler handler )
   {
     if ( auto ag = this->get_aggregator(id) )
@@ -210,8 +136,8 @@ namespace wrtstat {
     return nullptr;
   }
 
-  aggregator_registry::composite_pusher_t aggregator_registry::create_composite_pusher( 
-    id_t time_id, id_t read_id, id_t write_id, 
+  aggregator_registry::composite_pusher_t aggregator_registry::create_composite_pusher(
+    id_t time_id, id_t read_id, id_t write_id,
     aggregated_handler time_handler,
     aggregated_handler read_handler,
     aggregated_handler write_handler,
@@ -221,7 +147,7 @@ namespace wrtstat {
     simple_pusher_t time_pusher;
     simple_pusher_t read_pusher;
     simple_pusher_t write_pusher;
-    
+
     if ( auto ag = this->get_aggregator(time_id) )
       time_pusher = ag->create_simple_pusher(time_handler);
 
@@ -233,7 +159,7 @@ namespace wrtstat {
 
     if (time_pusher==nullptr && read_pusher==nullptr && write_pusher==nullptr)
       return nullptr;
-    
+
     return this->make_composite_handler_(time_pusher, read_pusher, write_pusher, summary_size);
   }
 
@@ -247,16 +173,16 @@ namespace wrtstat {
   {
     return this->create_data_pusher( this->create_aggregator(name, ts_now), handler);
   }
-  
+
   aggregator_registry::reduced_pusher_t aggregator_registry::create_reduced_pusher( const std::string& name, aggregated_handler handler, time_type ts_now )
   {
     return this->create_reduced_pusher(this->create_aggregator(name, ts_now), handler);
   }
-  
-  aggregator_registry::composite_pusher_t aggregator_registry::create_composite_pusher( 
-    const std::string& time_name, 
-    const std::string& read_name, 
-    const std::string& write_name, 
+
+  aggregator_registry::composite_pusher_t aggregator_registry::create_composite_pusher(
+    const std::string& time_name,
+    const std::string& read_name,
+    const std::string& write_name,
     aggregated_handler time_handler,
     aggregated_handler read_handler,
     aggregated_handler write_handler,
@@ -275,7 +201,7 @@ namespace wrtstat {
   void aggregator_registry::enable(bool value)
   {
     std::lock_guard<mutex_type> lk(_mutex);
-    
+
     this->_enabled = value;
     for (auto a : _agarr)
     {
@@ -295,6 +221,6 @@ namespace wrtstat {
     _agarr[ pos ] = nullptr;
     return this->_dict.free(id);
   }
- 
+
 
 }
