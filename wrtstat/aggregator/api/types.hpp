2f0f71fadaa0fd5558357dd2287273299023ce76
GitLab Runner <gitlab-runner@repobuild3.lan>
2021-04-21 00:36:54 +0300

    Generated export autocommit from mamba.ru

--- wrtstat/aggregator/api/types.hpp
+++ wrtstat/aggregator/api/types.hpp
@@ -13,7 +13,8 @@ typedef std::size_t id_t;
 typedef std::vector<value_type> data_type;
 typedef std::unique_ptr<data_type> data_ptr;
 
-static const id_t bad_id = static_cast<id_t>(-1);
+constexpr id_t bad_id = static_cast<id_t>(-1);
+constexpr id_t get_bad_id() { return bad_id; }
 
 enum class resolutions
 {
