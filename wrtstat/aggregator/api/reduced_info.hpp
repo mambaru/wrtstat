34c62bbd2e7c6bcd60daf24ddfa340e706259acb
GitLab Runner <gitlab-runner@repobuild4.lan>
2023-09-13 15:05:09 +0300

    Generated export autocommit from mamba.ru

--- wrtstat/aggregator/api/reduced_info.hpp
+++ wrtstat/aggregator/api/reduced_info.hpp
@@ -22,6 +22,9 @@ struct reduced_info
 
 inline reduced_info& reduced_info::operator += (const reduced_info& value)
 {
+  if ( this->ts == 0 )
+    this->ts = value.ts;
+
   if ( this->count+value.count != 0)
     this->avg = ( value_type(this->count)*this->avg + value_type(value.count)*value.avg) / value_type(this->count+value.count);
   else
