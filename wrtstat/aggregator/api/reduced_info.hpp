48265475b0bcc51bd65bace74c9a79267d322ff9
GitLab Runner <gitlab-runner@repobuild4.lan>
2023-09-16 16:12:26 +0300

    Generated export autocommit from mamba.ru

--- wrtstat/aggregator/api/reduced_info.hpp
+++ wrtstat/aggregator/api/reduced_info.hpp
@@ -23,17 +23,20 @@ struct reduced_info
 inline reduced_info& reduced_info::operator += (const reduced_info& value)
 {
   if ( this->ts == 0 )
-    this->ts = value.ts;
-
-  if ( this->count+value.count != 0)
-    this->avg = ( value_type(this->count)*this->avg + value_type(value.count)*value.avg) / value_type(this->count+value.count);
+  {
+    *this = value;
+  }
   else
-    this->avg = (this->avg + value.avg) / 2;
-  this->count += value.count;
-  this->lossy += value.lossy;
-  this->min = std::min(this->min, value.min);
-  this->max = std::min(this->max, value.max);
-  
+  {
+    if ( this->count+value.count != 0)
+      this->avg = ( value_type(this->count)*this->avg + value_type(value.count)*value.avg) / value_type(this->count+value.count);
+    else
+      this->avg = (this->avg + value.avg) / 2;
+    this->count += value.count;
+    this->lossy += value.lossy;
+    this->min = std::min(this->min, value.min);
+    this->max = std::min(this->max, value.max);
+  }
   return *this;
 }
 
